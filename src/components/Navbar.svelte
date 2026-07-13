3e8f249e3281ed991608e7de561160ea9c103864
heygideon <47083178+heygideon@users.noreply.github.com>
2026-07-13 22:45:53 +0100

    feat: add hero and navbar enter animations

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -53,20 +53,21 @@
         height: 32,
         priority: true,
       })}
-      <a href="/">
+      <a href="/" data-part="logo">
         <img {...attributes} class="h-8" />
         <span class="sr-only">Home</span>
       </a>
     {/if}
     <div class="flex-1"></div>
     <button
+      data-part="links"
       onclick={() => (mobileMenuOpenUser = true)}
       class="border border-transparent transition md:hidden"
     >
       <LuMenu class="size-6" />
       <span class="sr-only">Open navigation</span>
     </button>
-    <nav class="flex items-center gap-3 max-md:hidden">
+    <nav data-part="links" class="flex items-center gap-3 max-md:hidden">
       {#each links as link, idx}
         {#if idx > 0}
           <span role="separator" class="text-current/50">/</span>
@@ -134,18 +135,34 @@
 
 <svelte:window bind:scrollY bind:innerWidth />
 
-<!-- <script>
-  function onScroll() {
-    const header = document.getElementById("header");
-    if (!header) return;
+<style>
+  @media (prefers-reduced-motion: no-preference) {
+    [data-part="logo"] {
+      animation: logo 1s cubic-bezier(0.16, 1, 0.3, 1) 200ms both;
+    }
+    @keyframes logo {
+      0% {
+        translate: 16px 0;
+        opacity: 0;
+      }
+      100% {
+        translate: 0 0;
+        opacity: 1;
+      }
+    }
 
-    if (window.scrollY > 32) {
-      header.dataset.open = "true";
-    } else {
-      delete header.dataset.open;
+    [data-part="links"] {
+      animation: links 1s cubic-bezier(0.16, 1, 0.3, 1) 200ms both;
+    }
+    @keyframes links {
+      0% {
+        translate: -16px 0;
+        opacity: 0;
+      }
+      100% {
+        translate: 0 0;
+        opacity: 1;
+      }
     }
   }
-
-  window.addEventListener("scroll", onScroll);
-  onScroll();
-</script> -->
+</style>
